bb8cee86d9a04202db26b90b0ebfbbc351e6b04a
Cainã da Costa Jucá <caina.juca@industriall.ai>
2021-09-10 21:12:20 -0300

    feat: seed initial data

--- Database/Context/AcvContext.cs
+++ Database/Context/AcvContext.cs
@@ -1,7 +1,10 @@
 ﻿using Domain.Entities;
 using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Globalization;
 
 namespace Database.Context
 {
@@ -19,7 +22,57 @@ namespace Database.Context
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            var hasher = new PasswordHasher<AppUser>();
+
+            var AdminId = Guid.NewGuid().ToString();
+            var AdminRoleId = Guid.NewGuid().ToString();
+            var UserRoleId = Guid.NewGuid().ToString();
+
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>().HasData(
+                    new AppUser
+                    {
+                        Id = AdminId,
+                        FirstName = "Admin",
+                        LastName = "Admin",
+                        Email = "admin@admin.com",
+                        UserName = "admin@admin.com",
+                        NormalizedEmail = "ADMIN@ADMIN.COM",
+                        Registration = "11111111", // 8 chars
+                        BirthDate = DateTime.ParseExact("05031958", "ddMMyyyy", provider),
+                        Approved = true,
+                        EmailConfirmed = true,
+                        PasswordHash = hasher.HashPassword(null, "Psswrd@1.2?3!"),
+                        SecurityStamp = string.Empty
+                    }
+                );
+
+            builder.Entity<IdentityRole>().HasData(
+                    new IdentityRole
+                    {
+                        Id = AdminRoleId,
+                        Name = "Admin",
+                        NormalizedName = "ADMIN",
+                        ConcurrencyStamp = Guid.NewGuid().ToString()
+                    },
+                    new IdentityRole
+                    {
+                        Id = UserRoleId,
+                        Name = "User",
+                        NormalizedName = "USER",
+                        ConcurrencyStamp = Guid.NewGuid().ToString()
+                    }
+                );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                    new IdentityUserRole<string>
+                    {
+                        RoleId = AdminRoleId,
+                        UserId = AdminId
+                    }
+                );
         }
     }
 }
---
+++ Database/Migrations/20210910003705_SeedInitialData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "019f381f-cbd1-4b05-b3f5-00f1674ee5da");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "8ebcea5d-f33e-4326-a579-705e683b01c4", "52c991f9-845c-456d-b3b8-6007a316a9a6", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a3c082cd-80bd-4399-af34-38fff7bf104e", "659a9d22-7f95-4166-a0c8-0b95863e4ca7", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Approved", "BirthDate", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "Registration", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "2566de9d-766c-4ec5-ba72-626acb6fa29e", 0, true, new DateTime(1958, 3, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "ac3e16c3-e1cb-496c-9d3e-91b07118ec43", "admin@admin.com", true, "Admin", "Admin", false, null, "ADMIN@ADMIN.COM", null, "AQAAAAEAACcQAAAAENYxBusCGzg0NhwIRJWCKgDz4jqX6/cyzsRiZt1tj/sOfYxtuieMA+9bjHI3/SBGOw==", null, false, "11111111", "", false, "admin@admin.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "UserId", "RoleId" },
+                values: new object[] { "2566de9d-766c-4ec5-ba72-626acb6fa29e", "8ebcea5d-f33e-4326-a579-705e683b01c4" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a3c082cd-80bd-4399-af34-38fff7bf104e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "UserId", "RoleId" },
+                keyValues: new object[] { "2566de9d-766c-4ec5-ba72-626acb6fa29e", "8ebcea5d-f33e-4326-a579-705e683b01c4" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8ebcea5d-f33e-4326-a579-705e683b01c4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2566de9d-766c-4ec5-ba72-626acb6fa29e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Approved", "BirthDate", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "Registration", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "019f381f-cbd1-4b05-b3f5-00f1674ee5da", 0, false, new DateTime(1958, 3, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "761e6af4-085a-490c-80cf-e0bb560c578d", "admin@admin.com", true, "Admin", "", false, null, "ADMIN@ADMIN.COM", null, "AQAAAAEAACcQAAAAEBOrayTSMH/65RzJAL+sg8ODtyMnQW6MEFWMlw+w49zq9poA6SiQiugt4PKJSv4cgw==", null, false, "11111111", "", false, "admin@admin.com" });
+        }
+    }
+}
